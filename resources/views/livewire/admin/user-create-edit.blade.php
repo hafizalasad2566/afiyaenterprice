8add8778ecc322aa3710c07acd764dc1987e057a
Abhik <abhik.paul@webskitters.com>
2021-02-16 15:32:24 +0530

    required is added to user model

--- resources/views/livewire/admin/user-create-edit.blade.php
+++ resources/views/livewire/admin/user-create-edit.blade.php
@@ -1,29 +1,29 @@
 <x-admin.form-section submit="saveOrUpdate">
     <x-slot name="form">
                     <x-admin.form-group>
-                        <x-admin.lable value="First Name" />
+                        <x-admin.lable value="First Name" required />
                         <x-admin.input type="text" wire:model.defer="first_name" placeholder="First Name"  class="{{ $errors->has('first_name') ? 'is-invalid' :'' }}" />
                         <x-admin.input-error for="first_name" />
                     </x-admin.form-group>
                     <x-admin.form-group>
-                        <x-admin.lable value="Last Name" />
+                        <x-admin.lable value="Last Name"  required />
                         <x-admin.input type="text" wire:model.defer="last_name" placeholder="Last Name"  class="{{ $errors->has('last_name') ? 'is-invalid' :'' }}" />
                         <x-admin.input-error for="last_name" />
                     </x-admin.form-group>
                     <x-admin.form-group>
-                        <x-admin.lable value="Email" />
+                        <x-admin.lable value="Email" required />
                         <x-admin.input type="text" wire:model.defer="email" placeholder="Email" autocomplete="off" class="{{ $errors->has('email') ? 'is-invalid' :'' }}"/>
                         <x-admin.input-error for="email" />
                     </x-admin.form-group>
                     @if(!$isEdit)
                     <x-admin.form-group>
-                        <x-admin.lable value="Password" />
+                        <x-admin.lable value="Password"  required />
                         <x-admin.input type="password" wire:model.defer="password" placeholder="Password" autocomplete="off" class="{{ $errors->has('password') ? 'is-invalid' :'' }}"/>
                         <x-admin.input-error for="password" />
                     </x-admin.form-group>
 
                     <x-admin.form-group>
-                        <x-admin.lable value="Confirm Password" />
+                        <x-admin.lable value="Confirm Password"  required />
                         <x-admin.input type="password" wire:model.defer="password_confirmation" placeholder="Reenter Password" autocomplete="off" class="{{ $errors->has('password_confirmation') ? 'is-invalid' :'' }}"/>
                         <x-admin.input-error for="password_confirmation" />
                     </x-admin.form-group>
@@ -35,7 +35,7 @@
                                     <x-admin.dropdown-item  :value="$status['value']" :text="$status['text']"/>                          
                                 @endforeach
                         </x-admin.dropdown>
-                        <x-admin.input-error for="password_confirmation" />
+                        <x-admin.input-error for="active" />
                     </x-admin.form-group>
                     
             </div>
