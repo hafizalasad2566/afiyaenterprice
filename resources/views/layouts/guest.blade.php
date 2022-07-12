3a9448a6a116baa4f9dbb185c35dcb57987599a5
Gourab <gourab.roy@webskitters.com>
2022-07-12 14:33:09 +0530

    fav icon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="shortcut icon" href="{{asset('admin_assets/media/logos/favicon.ico')}}" />
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
